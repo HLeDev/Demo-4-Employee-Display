e2fc4ee78be514de05bb138bf58b3abd486880b1
Hung Le <Lexhung91@outlook.com>
2021-09-20 15:51:37 -0700

    Update

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo4EmpDisplay.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //13.  Create an array for names
+            ViewBag.Employeenames = new string[] { "Elena", "Logan", "Michael", "Nathan", "Sarah" };
+            //14.  Go to Employee Index page
+            return View();
+        }
+
+        //24.  Create Details method
+        public IActionResult Details(string empname)
+        {
+            ViewBag.selectedemployee = empname;
+            return View();
+        }
+        //25.  Go to Details.cshtml
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo4EmpDisplay
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //5.  Use MVC services
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+
+            //6.  Use mvc default route 
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //7.  MVC Default
+            app.UseMvcWithDefaultRoute();
+
+            //8.  Use Static Files
+            app.UseStaticFiles();
+            //9.  Create EmployeeController
+            //10.  Create Employee Folder in Views
+            //11.  Create Employee Index Page
+            //12.  Go to EmployeeController
+
+
+
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
